33ce7308f16609049a12dbf95b15807c0c5824c1
Anshul Mishra <anshulpayasi9695@gmail.com>
2022-11-22 15:59:20 +0530

    view product page desinged and updated

--- laravel8EcomPart20/my_ecom/app/Http/Controllers/Front/FrontController.php
+++ laravel8EcomPart20/my_ecom/app/Http/Controllers/Front/FrontController.php
@@ -95,4 +95,40 @@ class FrontController extends Controller
 
         return view('front.index',$result);
     }
+
+    public function product(Request $request,$slug){
+        $result['product']=
+        DB::table('products')
+        ->where(['status'=>1])
+        ->where(['slug'=>$slug])
+        ->get();
+
+    foreach($result['product'] as $list1){
+        $result['product_attr'][$list1->id]=
+            DB::table('products_attr')
+            ->leftJoin('sizes','sizes.id','=','products_attr.size_id')
+            ->leftJoin('colors','colors.id','=','products_attr.color_id')
+            ->where(['products_attr.products_id'=>$list1->id])
+            ->get();
+        
+    }
+
+    $result['releted_products']=
+    DB::table('products')
+    ->where(['status'=>1])
+    ->where('slug','!=',$slug)
+    ->where(['category_id'=>$result['product'][0]->category_id])
+    ->get();
+    foreach($result['releted_products'] as $list1){
+        $result['releted_products_attr'][$list1->id]=
+            DB::table('products_attr')
+            ->leftJoin('sizes','sizes.id','=','products_attr.size_id')
+            ->leftJoin('colors','colors.id','=','products_attr.color_id')
+            ->where(['products_attr.products_id'=>$list1->id])
+            ->get();
+        
+    }
+    //  prx($result);
+       return view('front.product',$result);
+    }
 }
---
+++ laravel8EcomPart20/my_ecom/resources/views/front/product.blade.php
@@ -0,0 +1,321 @@
+@extends('front/layout')
+
+@section('container')
+  <!-- product category -->
+  <section id="aa-product-details">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="aa-product-details-area">
+            <div class="aa-product-details-content">
+              <div class="row">
+                <!-- Modal view slider -->
+                <div class="col-md-5 col-sm-5 col-xs-12">                              
+                  <div class="aa-product-view-slider">                                
+                    <div id="demo-1" class="simpleLens-gallery-container">
+                      <div class="simpleLens-container">
+                        <div class="simpleLens-big-image-container"><a data-lens-image="{{asset('storage/media/'.$product[0]->image)}}" class="simpleLens-lens-image"><img src="{{asset('storage/media/'.$product[0]->image)}}" class="simpleLens-big-image"></a></div>
+                      </div>
+                      <div class="simpleLens-thumbnails-container">
+                          <a data-big-image="{{asset('storage/media/'.$product[0]->image)}}" data-lens-image="{{asset('storage/media/'.$product[0]->image)}}" class="simpleLens-thumbnail-wrapper" href="#">
+                            <img src="{{asset('storage/media/'.$product[0]->image)}}" width="50px">
+                          </a>                                    
+                          <!-- <a data-big-image="img/view-slider/medium/polo-shirt-3.png" data-lens-image="img/view-slider/large/polo-shirt-3.png" class="simpleLens-thumbnail-wrapper" href="#">
+                            <img src="{{asset('storage/media/'.$product[0]->image)}}">
+                          </a>
+                          <a data-big-image="img/view-slider/medium/polo-shirt-4.png" data-lens-image="img/view-slider/large/polo-shirt-4.png" class="simpleLens-thumbnail-wrapper" href="#">
+                            <img src="{{asset('storage/media/'.$product[0]->image)}}">
+                          </a> -->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- Modal view content -->
+                <div class="col-md-7 col-sm-7 col-xs-12">
+                  <div class="aa-product-view-content">
+                    <h3>{{$product[0]->name}}</h3>
+                    @if($product[0]->lead_time!='')
+                    <div class="aa-prod-quantity">
+                    <p class="aa-prod-category ">
+                        Lead Time: <a href="#">{{$product[0]->lead_time}}</a>
+                      </p>
+                    </div>
+                    @endif
+                    <div class="aa-price-block">
+                    <span class="aa-product-view-price" style="color:green">Rs  {{$product_attr[$product[0]->id][0]->price}}</span>
+                   <del> <span class="aa-product-view-price" style="color:red">Rs  {{$product_attr[$product[0]->id][0]->mrp}}</span></del>
+                     
+                      <p class="aa-product-avilability">Avilability: <span>In stock</span></p>
+                    </div>
+                    <p>{!! $product[0]->short_desc !!}</p>
+                    <h4>Size</h4>
+                    <div class="aa-prod-view-size">
+                    
+                      @foreach($product_attr[$product[0]->id] as $size)
+                           @if($size->size!='')
+                           <a href="#">{{$size->size}}</a>
+                      @endif
+                           @endforeach   
+                    </div>
+                    <h4>Color</h4>
+                    <div class="aa-color-tag">
+                        @foreach($product_attr[$product[0]->id] as $color)
+                           @if($color->color!='')
+                      <a href="#" class="aa-color-{{strtolower($color->color)}}"></a>
+                      @endif
+                           @endforeach            
+                    </div>
+                    <div class="aa-prod-quantity">
+                      <form action="">
+                        <select id="" name="">
+                          <option selected="1" value="0">1</option>
+                          <option value="1">2</option>
+                          <option value="2">3</option>
+                          <option value="3">4</option>
+                          <option value="4">5</option>
+                          <option value="5">6</option>
+                        </select>
+                      </form>
+                      <p class="aa-prod-category">
+                        Model: <a href="#">{{$product[0]->model}}</a>
+                      </p>
+                      <p class="aa-prod-category">
+                        Category: <a href="#">{{$product[0]->category_id}}</a>
+                      </p>
+    
+                    </div>
+                    <div class="aa-prod-view-bottom">
+                      <a class="aa-add-to-cart-btn" href="#">Add To Cart</a>
+                      <a class="aa-add-to-cart-btn" href="#">Wishlist</a>
+                      <a class="aa-add-to-cart-btn" href="#">Compare</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="aa-product-details-bottom">
+              <ul class="nav nav-tabs" id="myTab2">
+                <li><a href="#description" data-toggle="tab">Description</a></li>
+                <li><a href="#technical_specification" data-toggle="tab">Technical Specification</a></li>
+                <li><a href="#uses" data-toggle="tab">Uses</a></li>
+                <li><a href="#warranty" data-toggle="tab">Warranty</a></li>
+                <li><a href="#review" data-toggle="tab">Reviews</a></li>                
+              </ul>
+
+              <!-- Tab panes -->
+              <div class="tab-content">
+                <div class="tab-pane fade in active" id="description">
+                <p>
+                {!! $product[0]->desc !!}
+                </p>
+                </div>
+
+                <div class="tab-pane fade in active" id="technical_specification">
+                <p>
+                {!! $product[0]->technical_specification !!}
+                </p>
+                </div>
+                <div class="tab-pane fade in active" id="uses">
+                <p>
+                {!! $product[0]->uses !!}
+                </p>
+                </div>
+
+                <div class="tab-pane fade in active" id="warranty">
+                <p>
+                {!! $product[0]->warranty !!}
+                </p>
+                </div>
+                <div class="tab-pane fade " id="review">
+                 <div class="aa-product-review-area">
+                   <h4>2 Reviews for T-Shirt</h4> 
+                   <ul class="aa-review-nav">
+                     <li>
+                        <div class="media">
+                          <div class="media-left">
+                            <a href="#">
+                              <img class="media-object" src="img/testimonial-img-3.jpg" alt="girl image">
+                            </a>
+                          </div>
+                          <div class="media-body">
+                            <h4 class="media-heading"><strong>Marla Jobs</strong> - <span>March 26, 2016</span></h4>
+                            <div class="aa-product-rating">
+                              <span class="fa fa-star"></span>
+                              <span class="fa fa-star"></span>
+                              <span class="fa fa-star"></span>
+                              <span class="fa fa-star"></span>
+                              <span class="fa fa-star-o"></span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="media">
+                          <div class="media-left">
+                            <a href="#">
+                              <img class="media-object" src="img/testimonial-img-3.jpg" alt="girl image">
+                            </a>
+                          </div>
+                          <div class="media-body">
+                            <h4 class="media-heading"><strong>Marla Jobs</strong> - <span>March 26, 2016</span></h4>
+                            <div class="aa-product-rating">
+                              <span class="fa fa-star"></span>
+                              <span class="fa fa-star"></span>
+                              <span class="fa fa-star"></span>
+                              <span class="fa fa-star"></span>
+                              <span class="fa fa-star-o"></span>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </li>
+                   </ul>
+                   <h4>Add a review</h4>
+                   <div class="aa-your-rating">
+                     <p>Your Rating</p>
+                     <a href="#"><span class="fa fa-star-o"></span></a>
+                     <a href="#"><span class="fa fa-star-o"></span></a>
+                     <a href="#"><span class="fa fa-star-o"></span></a>
+                     <a href="#"><span class="fa fa-star-o"></span></a>
+                     <a href="#"><span class="fa fa-star-o"></span></a>
+                   </div>
+                   <!-- review form -->
+                   <form action="" class="aa-review-form">
+                      <div class="form-group">
+                        <label for="message">Your Review</label>
+                        <textarea class="form-control" rows="3" id="message"></textarea>
+                      </div>
+                      <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" placeholder="Name">
+                      </div>  
+                      <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="example@gmail.com">
+                      </div>
+
+                      <button type="submit" class="btn btn-default aa-review-submit">Submit</button>
+                   </form>
+                 </div>
+                </div>            
+              </div>
+            </div>
+            <!-- Related product -->
+            <div class="aa-product-related-item">
+              <h3>Related Products</h3>
+              <ul class="aa-product-catg aa-related-item-slider">
+                <!-- start single product item -->
+            
+                @if(isset($releted_products[0]))
+                       @foreach($releted_products as $productArr)
+                        <li>
+                          <figure>
+                            <a class="aa-product-img" href="{{url('product/'.$productArr->slug)}}"><img src="{{asset('storage/media/'.$productArr->image)}}" alt="{{$productArr->name}}"></a>
+                            <a class="aa-add-card-btn" href="{{url('product/'.$productArr->slug)}}"><span class="fa fa-shopping-cart"></span>Add To Cart</a>
+                            <figcaption>
+                              <h4 class="aa-product-title"><a href="{{url('product/'.$productArr->slug)}}">{{$productArr->name}}</a></h4>
+                              <span class="aa-product-price">Rs {{$releted_products_attr[$productArr->id][0]->price}}</span><span class="aa-product-price"><del>Rs {{$releted_products_attr[$productArr->id][0]->mrp}}</del></span>
+                            </figcaption>
+                          </figure>                          
+                        </li>  
+                        @endforeach    
+                        @else
+                        <li>
+                          <figure>
+                            No data found
+                          </figure>
+                        <li>
+                        @endif                 
+                                                                                        
+              </ul>
+              <!-- quick view modal -->                  
+              <div class="modal fade" id="quick-view-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">                      
+                    <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <div class="row">
+                        <!-- Modal view slider -->
+                        <div class="col-md-6 col-sm-6 col-xs-12">                              
+                          <div class="aa-product-view-slider">                                
+                            <div class="simpleLens-gallery-container" id="demo-1">
+                              <div class="simpleLens-container">
+                                  <div class="simpleLens-big-image-container">
+                                      <a class="simpleLens-lens-image" data-lens-image="{{asset('storage/media/'.$product[0]->image)}}">
+                                          <img src="{{asset('storage/media/'.$product[0]->image)}}" class="simpleLens-big-image">
+                                      </a>
+                                  </div>
+                              </div>
+                              <div class="simpleLens-thumbnails-container">
+                                  <a href="#" class="simpleLens-thumbnail-wrapper"
+                                     data-lens-image="img/view-slider/large/polo-shirt-1.png"
+                                     data-big-image="img/view-slider/medium/polo-shirt-1.png">
+                                      <img src="{{asset('storage/media/'.$product[0]->image)}}">
+                                  </a>                                    
+                                  <a href="#" class="simpleLens-thumbnail-wrapper"
+                                     data-lens-image="img/view-slider/large/polo-shirt-3.png"
+                                     data-big-image="img/view-slider/medium/polo-shirt-3.png">
+                                      <img src="{{asset('storage/media/'.$product[0]->image)}}">
+                                  </a>
+
+                                  <a href="#" class="simpleLens-thumbnail-wrapper"
+                                     data-lens-image="img/view-slider/large/polo-shirt-4.png"
+                                     data-big-image="img/view-slider/medium/polo-shirt-4.png">
+                                      <img src="{{asset('storage/media/'.$product[0]->image)}}">
+                                  </a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <!-- Modal view content -->
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <div class="aa-product-view-content">
+                            <h3>T-Shirt</h3>
+                            <div class="aa-price-block">
+                              <span class="aa-product-view-price">$34.99</span>
+                              <p class="aa-product-avilability">Avilability: <span>In stock</span></p>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis animi, veritatis quae repudiandae quod nulla porro quidem, itaque quis quaerat!</p>
+                            <h4>Size</h4>
+                            <div class="aa-prod-view-size">
+                              <a href="#">S</a>
+                              <a href="#">M</a>
+                              <a href="#">L</a>
+                              <a href="#">XL</a>
+                            </div>
+                            <div class="aa-prod-quantity">
+                              <form action="">
+                                <select name="" id="">
+                                  <option value="0" selected="1">1</option>
+                                  <option value="1">2</option>
+                                  <option value="2">3</option>
+                                  <option value="3">4</option>
+                                  <option value="4">5</option>
+                                  <option value="5">6</option>
+                                </select>
+                              </form>
+                              <p class="aa-prod-category">
+                                Category: <a href="#">Polo T-Shirt</a>
+                              </p>
+                            </div>
+                            <div class="aa-prod-view-bottom">
+                              <a href="#" class="aa-add-to-cart-btn"><span class="fa fa-shopping-cart"></span>Add To Cart</a>
+                              <a href="#" class="aa-add-to-cart-btn">View Details</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>                        
+                  </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+              </div>
+              <!-- / quick view modal -->   
+            </div>  
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- / product category -->
+@endsection
