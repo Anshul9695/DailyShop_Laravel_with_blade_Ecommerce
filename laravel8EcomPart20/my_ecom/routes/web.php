88500e99ac5b9a71bfc70491c1ae5db08736c182
Durgesh Chandra Mishra <durgesh.dev@tarte.com>
2023-02-07 07:29:13 +0530

    New Project Grocery_store Added

--- laravel8EcomPart20/my_ecom/routes/web.php
+++ laravel8EcomPart20/my_ecom/routes/web.php
@@ -12,7 +12,9 @@ use App\Http\Controllers\Admin\HomeBannerController;
 
 use App\Http\Controllers\Front\FrontController;
 use Illuminate\Support\Facades\Route;
+// Admin login and password   
 
+// email --> admin@gmail.com  --> vishal
 /* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
 
 Route::get('/', [FrontController::class , 'index']);
